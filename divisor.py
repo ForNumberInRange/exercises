8c27334b9a1807eab9a058746c0ac330ffe16b25
ForNumberInRange <j.gehring1@gmx.de>
2016-06-29 13:42:45 +0200

    divisor.py
    Hi dozie9, your code won't work this way. You need to change the format of the user input to an integer value like this: 
    
    num = input(int('Enter a number'))
    
    Also, why do you use num/2 in the range function?

--- divisor.py
+++ divisor.py
@@ -1,8 +1,8 @@
 __author__ = 'dozie9'
 
-num = input('Enter a number')
+num = int(input('Enter a number'))
 divList = []
-for i in range(1,num/2 + 1):
+for i in range(1,num + 1):
     if num%i == 0 :
         divList.append(i)
 print divList
